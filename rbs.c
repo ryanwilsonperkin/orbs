62d02434b19d115c9af8e696d5cf76345d27e834
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-10-31 16:58:36 -0400

    Parse command line options.

--- rbs.c
+++ rbs.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define ERROR(s) {fprintf(stderr, "error: %s\n", s); exit(EXIT_FAILURE);}
 int main(int argc, char *argv[])
@@ -10,10 +11,36 @@ int main(int argc, char *argv[])
     int tile_width = 0;
     int max_density = 0;
     int max_steps = 0;
-    int random_seed = 0;
+    int random_seed = time(NULL);
     bool interactive = false;
 
-    // Get args
+    for (int i = 1; i < argc; i++) {
+        switch(argv[i][0]) {
+            case 'p':
+                n_procs = atoi(argv[i]+1);
+                break;
+            case 'b':
+                board_width = atoi(argv[i]+1);
+                break;
+            case 't':
+                tile_width = atoi(argv[i]+1);
+                break;
+            case 'c':
+                max_density = atoi(argv[i]+1);
+                break;
+            case 'm':
+                max_steps = atoi(argv[i]+1);
+                break;
+            case 's':
+                random_seed = atoi(argv[i]+1);
+                break;
+            case 'i':
+                interactive = true;
+                break;
+            default:
+                ERROR("Unrecognized argument.");
+        }
+    }
     
     if (n_procs <= 0) ERROR("Thread count must be greater than 0.");
     if (board_width <= 1) ERROR("Board width must be greater than 1.");
