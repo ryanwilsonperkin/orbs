5b81477b0073a82e61f846cbf05e6797ef9a027c
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-11-01 16:32:12 -0400

    Initialize board in init_board.

--- board.c
+++ board.c
@@ -1,9 +1,22 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "board.h"
 
 void init_board(board *b, int board_width, int random_seed)
 {
+    srand(random_seed);
+    b->points = malloc(board_width * sizeof(int *));
+    for (int i = 0; i < board_width; i++) {
+        b->points[i] = malloc(board_width * sizeof(int));
+        for (int j = 0; j < board_width; j++) {
+            b->points[i][j] = rand() % 3;
+        }
+    }
+    b->width = board_width;
+    b->complete = false;
+    b->max_density = 0;
 }
 
 void free_board(board *b)
